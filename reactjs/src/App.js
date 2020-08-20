3f1a8e251c693233819d916f07284bee9f8e3720
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-08-20 11:56:53 -0300

    conceito de propriedades do react: criacao de props e a prop children

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -5,8 +5,15 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Item de Lista 1</li>
+          <li>Item de Lista 2</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <p>Aqui não tem ítens de lista</p>
+      </Header>
     </>
   );
 }
