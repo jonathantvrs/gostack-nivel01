bc4de1c8c718b09e9f35cf50dca0e65e83bb0a8f
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-08-21 18:35:44 -0300

    adicionando projeto mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+  const [projects, setProjects] = React.useState([]);
+
+  React.useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo project ${Date.now()}`,
+      owner: 'Jonathan Tavares',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#a2a" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+
+      <TouchableOpacity
+        activeOpacity={0.6}
+        style={styles.button}
+        onPress={handleAddProject}
+      >
+        <Text style={styles.buttonText}>Add project</Text>
+      </TouchableOpacity>
+      {/* <View style={styles.container}>
+        {projects.map((project) => (
+          <Text style={styles.project} key={project.id}>
+            {project.title}
+          </Text>
+        ))}
+      </View> */}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a2a',
+    /* justifyContent: 'center',
+    alignItems: 'center', */
+  },
+  project: {
+    color: '#fff',
+    fontSize: 30,
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+});
